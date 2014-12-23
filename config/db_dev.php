82821789812079a9a3147a9f32ac0a7b859b43cb
Azraf <c.azraf@gmail.com>
2014-12-23 23:44:38 +0600

    Update db_dev.php

--- config/db_dev.php
+++ config/db_dev.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=go_w3nav',
+    'dsn' => 'mysql:host=localhost;dbname=yii2webapp_dev',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
