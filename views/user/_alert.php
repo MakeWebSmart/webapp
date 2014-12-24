65e4e442339390c6780b1cbabc7bcda343651649
Azraf <c.azraf@gmail.com>
2014-12-25 02:04:39 +0600

    Update _alert.php

--- views/user/_alert.php
+++ views/user/_alert.php
@@ -1,4 +1,4 @@
-<!--<div class="row">
+<div class="row">
     <div class="col-xs-12">
         <?php foreach (Yii::$app->session->getAllFlashes() as $type => $message): ?>
             <?php if (in_array($type, ['success', 'danger', 'warning', 'info'])): ?>
@@ -8,4 +8,4 @@
             <?php endif ?>
         <?php endforeach ?>
     </div>
-</div>-->
+</div>
